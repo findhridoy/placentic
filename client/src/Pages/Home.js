14b52abafa7d392d79423554fd84f86746f59e49
findhridoy <find.hridoy546@gmail.com>
2022-04-06 23:52:24 +0600

    added home page

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../Components/Hero";
+import Layout from "../Layouts/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Hero />
+    </Layout>
+  );
+};
+
+export default Home;
