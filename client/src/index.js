a55cf1377e74d5329ac619de683bce6fa64c5f44
findhridoy <find.hridoy546@gmail.com>
2024-01-04 23:40:14 +0600

    fixed: index.scss

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,21 @@
 import { SnackbarProvider } from "notistack";
 import React from "react";
-// import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { store } from "./app/store";
-import "./sass/index.scss";
+import "./sass/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <SnackbarProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </SnackbarProvider>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <SnackbarProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SnackbarProvider>
+    </Provider>
+  </React.StrictMode>
 );
