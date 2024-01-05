b1e4c3b81f7109c4255e6480e8e50b7845f2528f
Saber Hossain Hridoy <93186266+findhridoy@users.noreply.github.com>
2024-01-05 21:50:15 +0600

    Delete client/src/Dashboard directory

--- client/src/Dashboard/DashboardPages/Dashboard.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DashboardLayout from "../DashboardLayout/DashboardLayout";
-
-const Dashboard = () => {
-  return <DashboardLayout>Dashboard</DashboardLayout>;
-};
-
-export default Dashboard;
